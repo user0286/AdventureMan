530dd17ca1862cb5e0b5a17ab6d4ce0768b14065
user0286 <150676845+user0286@users.noreply.github.com>
2024-01-22 20:11:42 +0700

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ const config = {
 		default: 'arcade',
 		arcade: {
 			gravity: { y: 200 },
+			debug: true
 		},
 	},
 	scene: [AdventureManScene],
--- src/scene/AdventureManScene.js
+++ src/scene/AdventureManScene.js
@@ -9,6 +9,7 @@ export default class AdventureManScene extends Phaser.Scene {
 
         //inisiasi player
         this.player = undefined
+        this.player1 = undefined
 
         //inisiasi move
         this.cursor = undefined
@@ -27,7 +28,7 @@ export default class AdventureManScene extends Phaser.Scene {
             {frameWidth:32, frameHeight:32}
         )
 
-        this.load.spritesheet('knight-run', 'images/Pixel Crawler - FREE - 1.8/Heroes/knight/Run/Run-Sheet',
+        this.load.spritesheet('knight-run', 'images/Pixel Crawler - FREE - 1.8/Heroes/Knight/Run/Run-Sheet.png',
             {frameWidth:64, frameHeight:64}
         )
 
@@ -48,20 +49,39 @@ export default class AdventureManScene extends Phaser.Scene {
         //Display player
         this.player = this.physics.add.sprite(100, 450, 'knight-idle').setOffset(-5,-20)
 
+
         //Bounds player
         this.player.setCollideWorldBounds(true)
+        
 
         //Collider
         this.physics.add.collider(this.player,this.platfrom)
+    
 
         //Cursor
         this.cursor = this.input.keyboard.createCursorKeys()
 
         //Create Animation
         this.createAnimation()
+
+        
     }
 
     update(){
+        if(!this.cursor.left.isDown && !this.cursor.right.isDown){
+            this.player.anims.play('turn', true)
+        }
+
+        if(this.cursor.right.isDown){
+            this.player.setVelocity(120, 450).setFlipX(false)
+            this.player.anims.play('right', true)
+        } else if(this.cursor.left.isDown){
+            this.player.setVelocity(-120, 450).setFlipX(true)
+            this.player.anims.play('left', true)
+        } else{
+            this.player.setVelocityX(0)
+        }
+
 
     }
 
@@ -70,6 +90,24 @@ export default class AdventureManScene extends Phaser.Scene {
             key: 'turn',
             frames: this.anims.generateFrameNumbers('knight-idle',
                 {start:0 , end: 3}),
+            frameRate: 10,
+            repeat: -1
+        })
+
+        //animtion to the right
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('knight-run',
+                {start:0, end:5}),
+            frameRate: 10,
+            repeat: -1
+        })
+
+        //animation to the left
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('knight-run',
+                {start:0, end:5}),
             frameRate: 10,
             repeat: -1
         })
