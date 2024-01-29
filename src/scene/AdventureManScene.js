b391be00b7b2b3c0001f4e9576e4f17901458ad1
user0286 <150676845+user0286@users.noreply.github.com>
2024-01-29 18:24:12 +0700

    Add files via upload

--- src/scene/AdventureManScene.js
+++ src/scene/AdventureManScene.js
@@ -5,7 +5,13 @@ export default class AdventureManScene extends Phaser.Scene {
 		super('adventure-man-scene')
 	}
     init(){
-        this.platfrom = undefined
+        //inisiasi Ground
+        this.ground = undefined
+
+        //inisiasi Platform
+        this.platform2 = undefined
+        this.platform3 = undefined
+        this.platform4 = undefined
 
         //inisiasi player
         this.player = undefined
@@ -13,16 +19,30 @@ export default class AdventureManScene extends Phaser.Scene {
 
         //inisiasi move
         this.cursor = undefined
+
+        //inisiasi Malabulu
+        this.Malabulu = undefined
     }
 
     preload(){
         this.load.image('background', 'images/Tiles/Assets/Background_2.png')
 
         //plafrom
-        this.load.spritesheet('platfrom', 'images/Tiles/Assets/Assets.png', {
+        this.load.spritesheet('ground', 'images/Tiles/Assets/Assets.png', {
             frameWidth: 32, frameHeight: 32
         })
 
+        this.load.spritesheet('platform2', 'images/Tiles/Assets/Assets.png', {
+            frameWidth: 32, frameHeight: 32
+        })
+
+        this.load.spritesheet('platform3', 'images/Tiles/Assets/Assets.png', {
+            frameWidth: 32, frameHeight: 32
+        })
+
+        this.load.spritesheet('platform4', 'images/Tiles/Assets/Assets.png', {
+            frameWidth: 32, frameHeight: 32
+        })
         //Heroes
         this.load.spritesheet('knight-idle', 'images/Pixel Crawler - FREE - 1.8/Heroes/Knight/Idle/Idle-Sheet.png',
             {frameWidth:32, frameHeight:32}
@@ -32,31 +52,58 @@ export default class AdventureManScene extends Phaser.Scene {
             {frameWidth:64, frameHeight:64}
         )
 
+        //Malabulu
+        this.load.image('malabulu', 'images/Malabulu.png')
     }
 
     create(){
         //Display Background
         this.add.image(400, 300, 'background').setScale(2.3)
 
-        //Display Platfrom
-        this.platfrom = this.physics.add.staticGroup({
-            key: 'platfrom',
+        //Display ground
+        this.ground = this.physics.add.staticGroup({
+            key: 'ground',
             frame: 0,
             repeat: 85,
             setXY:{ x: 0, y:585, stepX: 10}
         })
 
-        //Display player
-        this.player = this.physics.add.sprite(100, 450, 'knight-idle').setOffset(-5,-20)
+        //display Platform
+        this.platform2 = this.physics.add.staticGroup({
+            key: 'platform2',
+            frame: 0,
+            repeat: 65,
+            setXY:{ x: 500, y:500, stepX: 10}
+        })
 
+        this.platform3 = this.physics.add.staticGroup({
+            key: 'platform3',
+            frame: 0,
+            repeat: 65,
+            setXY:{ x: 0, y:400, stepX: 5}
+        })
+
+        this.platform4 = this.physics.add.staticGroup({
+            key: 'platform4',
+            frame: 0,
+            repeat: 65,
+            setXY:{ x: 450, y:300, stepX: 10}
+        })
+
+        //Display player
+        this.player = this.physics.add.sprite(369, 450, 'knight-idle').setOffset(-5,-20)
 
         //Bounds player
         this.player.setCollideWorldBounds(true)
-        
 
         //Collider
-        this.physics.add.collider(this.player,this.platfrom)
-    
+        this.physics.add.collider(this.player,this.ground)
+
+        this.physics.add.collider(this.player, this.platform2)
+
+        this.physics.add.collider(this.player, this.platform3)
+
+        this.physics.add.collider(this.player, this.platform4)
 
         //Cursor
         this.cursor = this.input.keyboard.createCursorKeys()
@@ -64,7 +111,14 @@ export default class AdventureManScene extends Phaser.Scene {
         //Create Animation
         this.createAnimation()
 
-        
+        //Malabulu
+        this.Malabulu = this.physics.add.group({
+            key: 'malabulu',
+            repeat: 0,
+            setXY: {x:50, y:0, stepX: 70}
+        })
+
+        this.physics.add.collider(this.Malabulu, this.platform2)
     }
 
     update(){
@@ -82,7 +136,10 @@ export default class AdventureManScene extends Phaser.Scene {
             this.player.setVelocityX(0)
         }
 
-
+        if(this.cursor.up.isDown) {
+            this.player.setVelocity(0, -200)
+            this.player.anims.play('turn')
+        }
     }
 
     createAnimation(){
